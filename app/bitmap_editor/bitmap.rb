284caa575277822015ea95c7a456612540bb517a
Iain McNulty <iainmcnulty@gmail.com>
2016-06-26 20:24:43 +0100

    Validate dimensions of Bitmap

--- app/bitmap_editor/bitmap.rb
+++ app/bitmap_editor/bitmap.rb
@@ -1,13 +1,28 @@
 module BitmapEditor
   class Bitmap
+    VALID_DIMENSIONS = (1..250)
+
     def initialize(height, width)
+      @height = height
+      @width = width
+      validate_args
       @grid = new_grid(height, width)
     end
 
     private
 
+    attr_reader :width, :height
+
     def new_grid(height, width)
       Array.new(width) { Array.new(height, 'O') }
+    end
+
+    def validate_args
+      raise ArgumentError, 'dimensions should be between 1 and 250' unless valid_dimensions?
+    end
+
+    def valid_dimensions?
+      VALID_DIMENSIONS.include?(height) && VALID_DIMENSIONS.include?(width)
     end
   end
 end
--- spec/bitmap_editor/bitmap_spec.rb
+++ spec/bitmap_editor/bitmap_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 require 'bitmap'
 
 RSpec.describe BitmapEditor::Bitmap do
+  describe 'initialize' do
+    it 'raises noe error if dimensions are outside 1 to 250' do
+      expect { described_class.new(1, 1) }.not_to raise_error
+    end
+
+    it 'raises error if dimensions are outside 1 to 250' do
+      expect { described_class.new(1, 0) }.to raise_error(ArgumentError)
+    end
+  end
 end
